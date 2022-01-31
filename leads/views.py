a2bd5c30ca42e4cd7480f4cb2080b0dc1b53ad1f
abhishek-T99 <timilsina.abhishek55@gmail.com>
2022-02-01 04:06:16 +0545

    context and template syntax

--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Lead
 
 def home_page(request):
+    
+    leads = Lead.objects.all()
+    
     # return HttpResponse('<h1>hello world</h1>')
-    return render(request, "leads/homepage.html")
-    #return render(request, "second_page.html")
+    
+    context = {
+        "leads": leads
+    }
+    
+    # return render(request, "leads/homepage.html")
+    return render(request, "second_page.html", context)
