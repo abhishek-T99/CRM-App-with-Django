9650c7cc34b699a481e1555b9d96db5b32144ab3
abhishek-T99 <timilsina.abhishek55@gmail.com>
2022-02-01 23:42:07 +0545

    added tailwind css

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,9 +1,13 @@
+import imp
 from tkinter.font import names
 from django.contrib import admin
 from django.urls import path, include
 
+from leads.views import landing_page
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leads/', include('leads.urls', namespace="leads"))
+    path('', landing_page, name='landing-page'),
+    path('leads/', include('leads.urls', namespace="leads")),
 ]
