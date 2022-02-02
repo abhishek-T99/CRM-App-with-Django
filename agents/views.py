253f6d37452e29a857fb736aff64d15540e2bdd9
abhishek-T99 <timilsina.abhishek55@gmail.com>
2022-02-03 02:38:15 +0545

    added agent

---
+++ agents/views.py
@@ -0,0 +1,12 @@
+from distutils.log import Log
+import imp
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from leads.models import Agent
+
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    template_name = "agents/agent_list.html"
+
+    def get_queryset(self):
+        return Agent.objects.all()        
