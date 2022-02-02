69ca50e92495fa53ec581923942cd911fab40865
abhishek-T99 <timilsina.abhishek55@gmail.com>
2022-02-03 00:51:49 +0545

    added authentication

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -128,3 +128,5 @@ AUTH_USER_MODEL = 'leads.User'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # "django.core.mail.backends.smtp.EmailBackend"
+
+LOGIN_REDIRECT_URL = "/leads"
